6f3bc8d9d002b6cbe4a24f63002f7ff4b91ce234
PseudoCoder11 <jenishkatuwal7@gmail.com>
2024-10-01 13:20:26 +0545

    Completed Calculator

--- projects/4-calculator-version-final/calculator/src/App.jsx
+++ projects/4-calculator-version-final/calculator/src/App.jsx
@@ -5,7 +5,17 @@ import { useState } from "react";
 
 function App() {
   const [calVal, setCalVal] = useState("");
-  const onButtonClick = (buttonText) => console.log(buttonText);
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "C") {
+      setCalVal("");
+    } else if (buttonText === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newDisplayValue = calVal + buttonText;
+      setCalVal(newDisplayValue);
+    }
+  };
   return (
     <div className={styles.calculator}>
       <Display displayValue={calVal} />
